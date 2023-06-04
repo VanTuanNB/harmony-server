aeee1346a2941af9ae8de510f67ed0847d2d8dc3
JosephHuy1906 <huydeptrai19062002@gmail.com>
2023-06-04 15:51:54 +0700

    add all task login signup by google

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 
 import IsRequirementReq from '@/decorators/IsRequirementReq.decorator';
 import AuthService from '@/services/auth.service';
+import IUser from '@/constraints/interfaces/IUser';
 
 export default class AuthController {
     @IsRequirementReq(['email', 'password'], 'body')
@@ -12,5 +13,14 @@ export default class AuthController {
         const payload: { email: string; password: string } = req.body;
         const loginService = await AuthService.loginForm(payload);
         return res.status(loginService.status).json(loginService);
+    }
+
+    public static async loginPassport(
+        req: Request,
+        res: Response
+    ): Promise<Response | void> {
+        const email = req.user as IUser;
+        const loginServiceGGFB = await AuthService.loginGGFB(email);
+        return res.status(loginServiceGGFB.status).json(loginServiceGGFB);
     }
 }
--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,9 +1,17 @@
 import { Router } from 'express';
 
 import AuthController from '@/controllers/auth.controller';
+import passport from 'passport';
+import '@/configs/passportGoogle.config';
 
 const router: Router = Router();
 
 router.post('/loginForm', AuthController.loginForm);
+
+router.get('/google',
+    passport.authenticate('google', { scope: ['profile', 'email'], session: false }));
+
+router.get('/google/callback',
+    passport.authenticate('google', { session: false }), AuthController.loginPassport);
 
 export default router;
