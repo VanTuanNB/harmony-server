9cf87ced929160b1eec6b8139b3c4ef3c33ad999
VanTuanNB <vantuan26072002@gmail.com>
2023-08-13 16:25:38 +0700

    [HMS-45] get api suggess song paging

--- src/controllers/song.controller.ts
+++ src/controllers/song.controller.ts
@@ -34,6 +34,21 @@ export default class SongController {
         return res.status(song.status).json(song);
     }
 
+    public async suggest(
+        req: Request,
+        res: Response,
+    ): Promise<Response | void> {
+        const { page, size } = req.query as {
+            page?: string;
+            size?: string;
+        };
+        const suggestService = await songService.suggest(
+            Number.parseInt(page || '1'),
+            Number.parseInt(size || '10'),
+        );
+        return res.status(suggestService.status).json(suggestService);
+    }
+
     @IsRequirementReq('id', 'params')
     public async getStreamSong(
         req: Request,
